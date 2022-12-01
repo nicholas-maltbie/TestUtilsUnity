b79d94fc3ee926897aaeb72a449f53f56b28ea89
Nick Maltbie <nick.dmalt@gmail.com>
2022-11-30 22:58:44 -0800

    Overridable Unity Service (#4)

--- Packages/com.nickmaltbie.TestUtilsUnity/Scripts/UnityService.cs
+++ Packages/com.nickmaltbie.TestUtilsUnity/Scripts/UnityService.cs
@@ -16,29 +16,28 @@
 // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using UnityEngine;
+
 namespace nickmaltbie.TestUtilsUnity
 {
     /// <summary>
-    /// Interface for managing static information from unity classes.
+    /// Default implementation of unity service
     /// </summary>
-    public interface IUnityService
+    public class UnityService : IUnityService
     {
         /// <summary>
-        /// Get the current delta time in seconds between this and last frame
+        /// Global instance of default UnityService.
         /// </summary>
-        /// <returns>The current delta time between this and the previous frame</returns>
-        float deltaTime { get; }
+        /// <returns></returns>
+        public static IUnityService Instance { get; } = new UnityService();
 
-        /// <summary>
-        /// Get the current fixed delta time for physics based update
-        /// </summary>
-        /// <returns>the delta time shown by the fixed delta time</returns>
-        float fixedDeltaTime { get; }
+        /// <inheritdoc/>
+        public float deltaTime => Time.deltaTime;
 
-        /// <summary>
-        /// Gets the current time in seconds since start of the game
-        /// </summary>
-        /// <value>Time in seconds since the start of the game</value>
-        float time { get; }
+        /// <inheritdoc/>
+        public float time => Time.time;
+
+        /// <inheritdoc/>
+        public float fixedDeltaTime => Time.fixedDeltaTime;
     }
 }
